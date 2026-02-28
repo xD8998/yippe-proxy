49946d5e63d44b12ce3a0878c19e8592949f49a8
xD8998 <108443149+xD8998@users.noreply.github.com>
2026-02-28 16:27:49 -0700

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,9 @@
+importScripts('/uv/uv.bundle.js');
+importScripts('/uv.config.js');
+importScripts('/uv/uv.sw.js');
+
+const sw = new UVServiceWorker();
+
+self.addEventListener('fetch', event => {
+    event.respondWith(sw.fetch(event));
+});
